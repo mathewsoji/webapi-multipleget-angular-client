80d477e768d839896a41026e6d7c56cb8455cf91
mathewsoji <soji.mathew@outlook.com>
2014-11-07 08:08:45 +0530

    Adding...

---
+++ webapi-multipleget-angular-client/WebAPI_Demo/Controllers/MathController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WepAPI_Demo.Controllers
+{
+    public class MathController : ApiController
+    {
+
+        [HttpGet]
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        [HttpGet]
+        public int Substract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        [HttpGet]
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        [HttpGet]
+        public int Divide(int value1, int value2)
+        {
+            if ( (value1 == 0) || (value2==0))
+            {
+                HttpResponseMessage ex = new HttpResponseMessage();
+                ex.StatusCode = HttpStatusCode.BadRequest;
+                ex.ReasonPhrase = "Value can not be zero!";
+                throw new HttpResponseException(ex);
+            }
+            else
+            {
+                return value1 / value2;
+            }
+          
+        }
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return "default";
+           
+        }
+
+
+
+    }
+}
---
+++ webapi-multipleget-angular-client/WebAPI_Demo/Web/scripts/app/myapp.js
@@ -0,0 +1,38 @@
+﻿var app = angular.module("myNoteApp", []);
+
+app.controller("myNoteCtrl", function ($scope, $http) {
+    $scope.message = "";
+    $scope.left = function () { return 100 - $scope.message.length; };
+    $scope.clear = function () { $scope.message = ""; };
+    $scope.save = function () { alert("Note Saved"); };
+
+ 
+    $scope.myclick = function (methcalled) {
+       
+   
+         //var urlString = "http://localhost:53157/api/math/get";
+        var urlString = "/api/math/"
+         var arg = "value1=" + $scope.Value1 + "&value2=" + $scope.Value2;
+         urlString = urlString + methcalled +  "/?" + arg;
+      
+        $http.get(urlString)
+                       .success(function (data) {
+                           var myjson = JSON.parse(data);
+                           $scope.APIResult = JSON.parse(myjson);
+
+                       })
+         .error(function (ex) {
+             
+             $scope.APIResult = "Error : " + ex.message;
+
+         })
+
+
+
+    };
+
+}); // eof app.controller
+
+   
+
+  
